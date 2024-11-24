3b95ba5d4851ea913de78c5d84c76fe5e7ef7b7a
xxfredy93xx <fredy.godoyrodriguez@georgebrown.ca>
2024-11-24 13:30:24 -0500

    Create lab12.sql

---
+++ docs/lab12.sql
@@ -0,0 +1,52 @@
+--adding a comment about a comment :p
+
+SELECT *
+FROM courses
+
+INSERT INTO assignments (course_id, title, status) 
+VALUES ('COMP1238', 'Assignment with no date', 'Not Started');
+
+SELECT * FROM assignments
+LIMIT 10; 
+
+SELECT count(*) FROM courses;
+
+SELECT min(due_date) FROM assignments;
+
+SELECT *
+FROM  courses
+WHERE course_name LIKE 'Intro%';
+
+SELECT sqlite_version();
+
+SELECT upper('ABCxyz');
+
+SELECT length('abcde');
+
+SELECT 7*5;
+
+SELECT concat('ABC', '-', 'xyz');
+
+SELECT date();
+
+SELECT strftime('%Y', due_date) AS Year, * 
+FROM assignments;
+
+SELECT DISTINCT SUBSTRING(course_id, 1, 4) 
+FROM courses;
+
+SELECT SUBSTRING(course_id, 1, 4) AS prefix, count(*)
+FROM courses
+GROUP BY prefix;
+
+SELECT *
+FROM  assignments
+WHERE status != 'Completed'
+ORDER BY due_date;
+
+SELECT course_id, title, status, due_date
+FROM assignments
+WHERE status != 'Completed'	
+  AND course_id LIKE 'COMM%'
+  AND due_date < '2024-12-31'
+ORDER BY due_date;
